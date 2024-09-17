118c67429aa465f15a0cfbfb716b0d506ed1c45c
chris <rckoenig@gmx.de>
2024-09-17 15:37:54 +0200

    Remove Secrets

--- examples/resources/resource.tf
+++ examples/resources/resource.tf
@@ -13,9 +13,5 @@ terraform {
 }
 
 provider "fabric" {
-  base_url        = "api.fabric.microsoft.com"
-  client_secret   = "5Og8Q~schfyvm2UfxZJIesL23ynPdvm~HmCg5bE5"
-  client_id       = "227f31e9-97ff-4fef-933d-c86c671d766a"
-  tenant_id       = "08e2c9c5-f65a-4cd8-8d35-a02272e142d6"
-  subscription_id = "8c62f590-4f16-4727-a2a7-f7a0304f308d"
+
 }
