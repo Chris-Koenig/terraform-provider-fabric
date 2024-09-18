e3eb770d42560be1416a01619b07d673adffc86f
chris <rckoenig@gmx.de>
2024-09-18 13:30:52 +0200

    Update

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -10,8 +10,8 @@ terraform {
 }
 
 provider "fabric" {
-  client_secret   = "5Og8Q~schfyvm2UfxZJIesL23ynPdvm~HmCg5bE5"
-  client_id       = "227f31e9-97ff-4fef-933d-c86c671d766a"
-  tenant_id       = "08e2c9c5-f65a-4cd8-8d35-a02272e142d6"
-  subscription_id = "8c62f590-4f16-4727-a2a7-f7a0304f308d"
+  client_secret   = ""
+  client_id       = ""
+  tenant_id       = ""
+  subscription_id = ""
 }
