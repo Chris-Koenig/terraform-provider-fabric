98e3d4b6c4a5968cde889022741bdb7e9128fd07
chris <rckoenig@gmx.de>
2024-09-24 16:51:57 +0200

    Update package and files names for fabric_role_assignment

--- examples/resources/fabric_role_assignment/resource.tf
+++ examples/resources/fabric_role_assignment/resource.tf
@@ -1,4 +1,4 @@
-resource "workspace_role_assignment" "example" {
+resource "fabric_role_assignment" "example" {
   workspace_id = "00000000-0000-0000-0000-000000000000" # Required: Replace with the actual workspace ID
   role         = "Member"                               # Required: Role assigned to the principal, e.g., "Member" or "Admin"
 
